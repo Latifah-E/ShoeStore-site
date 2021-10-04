fa276a96fd2914f8b6717359e600579653cac3d0
Smartcodes <smartcodes@Smartcodess-iMac.local>
2021-10-04 17:59:21 +0300

    manipulated api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\ShoesController;
 use App\Http\Controllers\CategoryController;
 use App\Models\category;
+use App\Models\Shoes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,9 @@ Route::resource('shoes', ShoesController::class);
 Route::resource('categories', CategoryController::class);
 Route::get('category_id={id}',function($id){
     return category::find($id)->shoes;
+});
+Route::get('brand={brand}',function($brand){
+    return Shoes::find($brand);
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
